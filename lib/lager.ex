8571c4da8d32fe7edfa134292de04b45f58bbd13
Yurii Rashkovskii <yrashk@spawngrid.com>
2012-06-02 16:59:04 -0700

    Initial implementation

---
+++ lib/lager.ex
@@ -0,0 +1,21 @@
+defmodule Lager do
+  levels = [:debug, :info, :notice, :warning, :error, :critical, :alert, :emergency]
+
+  lc level in levels do
+    contents =
+    quote do
+      def unquote(level).(msg) do
+        log(unquote(level), msg)
+      end
+      def unquote(level).(format, args)  do
+        log(unquote(level), format, args)
+      end
+    end
+    Module.eval_quoted __MODULE__, contents, file: __FILE__, line: __ENV__.line
+  end
+
+  def log(level, msg), do: :lager.log(level, Process.self, msg)
+  def log(level, format, args) when is_list(format), do: :lager.log(level, Process.self, format, args)
+  def log(level, format, args) when is_binary(format), do: log(level, binary_to_list(format), args)
+
+end
