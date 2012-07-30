6a32e562efd6b424abacfbb4eb13f8f6ab1ca90f
Yurii Rashkovskii <yrashk@gmail.com>
2012-07-30 11:05:49 -0700

    Updates mix.exs

--- mix.exs
+++ mix.exs
@@ -1,10 +1,17 @@
-defmodule Mix.Project do
+defmodule Lagerex.MixFile do
+  use Mix.Project
 
-   def project do
-     [project: "Lagerex", version: "0.1", 
+  def project do
+    [app: :lagerex,
+     project: "Lagerex", version: "0.1", 
+     deps: deps,
      compile_options: [ignore_module_conflict: true, docs: true]]
-   end
+  end
 
-   def application, do: []
+  def application, do: []
+
+  defp deps do
+    [{:lager, %r(.*), git: "https://github.com/basho/lager"}]
+  end
 
 end
