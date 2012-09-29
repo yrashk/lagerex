1cae155cc16f07410fe740515b57eefea182180f
Yurii Rashkovskii <yrashk@spawngrid.com>
2012-09-28 23:20:41 -0700

    Add expm package.exs

---
+++ package.exs
@@ -0,0 +1,4 @@
+Expm.Package.new(name: "lagerx", description: "Lightweight Lager 'binding'",
+                 version: "0.1", keywords: ["logging", "server"],
+                 maintainers: [[name: "Yurii Rashkovskii", email: "yrashk@gmail.com"]],
+                 repositories: [[github: "yrashk/lagerx", tag: "0.1"]])
